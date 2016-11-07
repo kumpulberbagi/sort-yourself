8403f728271e6180d5e8754d1f6d479223fe611d
Your Name <name@example.com>
2016-11-07 11:13:12 +0700

    Keterangan Program

--- selection_sort.js
+++ selection_sort.js
@@ -1,32 +1,28 @@
 'use strict'
 
-function selectionSort(arrays){
+function selectionSort(arrays) {
+
+
+
 }
 
 // Driver code
 function main() {
-
-  sort_from_file('random_wordlist.txt')
-  sort_from_file('reversed_wordlist.txt')
-  sort_from_file('sorted_wordlist.txt')
-
+    sort_from_file('random_wordlist.txt')//Menjalankan Fungsi sort_from_file() dengan data param random_wordlist.txt
+    sort_from_file('reversed_wordlist.txt')//Menjalankan Fungsi sort_from_file() dengan data param reversed_wordlist.txt
+    sort_from_file('sorted_wordlist.txt')//Menjalankan Fungsi sort_from_file() dengan data param sort_from_file.txt
 }
 
 function sort_from_file(filename) {
-  let fs = require('fs')
-
-  fs.readFile(filename, (err, data) => {
-
-    if (err)
-      return console.log(err)
-
-    let items = data.toString().split("\r\n")
-
-    console.log(filename)
-    console.log("--------")
-    console.log(selectionSort(items))
-
-  })
+    let fs = require('fs')
+    fs.readFile(filename, (err, data) => {
+        if (err)
+            return console.log(err)
+        let items = data.toString().split("\r\n")
+        console.log(filename)
+        console.log("--------")
+        console.log(selectionSort(items))
+    })
 }
 
-main()
+main()//Menjalankan fungsi main()/
